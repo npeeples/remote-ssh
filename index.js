390440a41a578c73bbe6c77b7cd46005e20e865b
Nick Peeples <npeeples@myvolly.com>
2020-04-30 14:36:16 -0500

    adding ability to be account aware and change keys used

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const { exec, spawn } = require('child_process')
+const accounts = function() {
+  try {
+    return require('./.accounts')
+  } catch {
+    console.error('could not find .accounts.js file containing key/host information')
+    process.exit(1)
+  }
+}()
+
+const { spawn } = require('child_process')
 
 const _ = require('lodash')
-const fuzzy = require('fuzzy');
+const fuzzy = require('fuzzy')
 
 const inquirer = require('inquirer')
 inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'))
@@ -33,7 +42,7 @@ function makePromptChoices(list) {
 
 function searchChoices(list) {
   return function(answers, input) {
-    input = input || '';
+    input = input || ''
     return new Promise(resolve => {
       var fuzzyResult = fuzzy.filter(input, list, { extract: i => i.name });
       resolve(fuzzyResult.map(el => el.original))
@@ -42,22 +51,26 @@ function searchChoices(list) {
 }
 
 async function main() {
+  let sts = new AWS.STS()
+  let ident = await sts.getCallerIdentity().promise()
+
   let ec2 = new AWS.EC2()
   let instances = await ec2.describeInstances().promise()
+
   let list = instances.Reservations.map(x => parseInstance(x.Instances[0]))
   list = _.sortBy(list, 'Name')
 
   inquirer.prompt([
     { type: 'autocomplete', name: 'server', message: 'Pick a server', source: searchChoices(makePromptChoices(list)) },
   ]).then(answers => {
-    // console.log('you want to login to', answers)
     // sshrc -i ~/.ssh/volly-ma-prod ec2-user@${answer#*,} -t "sudo script -q -c 'screen -dR -c /tmp/.np.screenrc np' /dev/null"
     // spawn('sshrc', ['-i', '~/.ssh/volly-ma-prod', `ec2-user@${answers['Pick server'].PrivateIpAddress}`, '-t', `"sudo script -q -c 'screen -dR -c /tmp/.np.screenrc np' /dev/null"`], [null, null, null, 'ipc']).unref();
     // exec(`sshrc -i ~/.ssh/volly-ma-prod ec2-user@${answers['Pick server'].PrivateIpAddress} -t "sudo script -q -c 'screen -dR -c /tmp/.np.screenrc np' /dev/null"`).unref();
-    // console.log(`ssh -i ~/.ssh/volly-ma-prod ec2-user@${answers['Pick server'].PrivateIpAddress} -t "sudo script -q -c 'screen -dR -c /tmp/.np.screenrc np' /dev/null"`)
-    // console.log(answers['Pick server'].PrivateIpAddress)
-    spawn('sshrc', ['-i', '~/.ssh/volly-ma-prod', `ec2-user@${answers['server'].PrivateIpAddress}`, '-t', `"sudo script -q -c \\'screen -dR -c /tmp/.np.screenrc np\\' /dev/null"`], { stdio: 'inherit' });
-  });
+
+    let acc = accounts[ident.Account]
+    let key = acc.key || '~/.ssh/volly-ma-prod'
+    spawn('sshrc', ['-i', key, `ec2-user@${acc.host || ''}${answers['server'].PrivateIpAddress}`, '-t', `"sudo script -q -c \\'screen -dR -c /tmp/.np.screenrc np\\' /dev/null"`], { stdio: 'inherit' })
+  })
 }
 
 main()
